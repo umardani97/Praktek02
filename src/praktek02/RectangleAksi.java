fba4e4a12d4513404fb1ef0e3724dcc1fe551fbc
umardani97 <32365705+umardani97@users.noreply.github.com>
2018-04-03 00:38:22 +0800

    memanggil method/behavior

--- src/praktek02/RectangleAksi.java
+++ src/praktek02/RectangleAksi.java
@@ -4,6 +4,8 @@ public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
         r1.panjang = 9;
-        r1.lebar = 3; 
+        r1.lebar = 3;
+        
+        r1.cetakInfo();
     }
 }
