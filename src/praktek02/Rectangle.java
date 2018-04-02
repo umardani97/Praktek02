b6b5a4d84282d0bf66e4ff2987161db76d61a61e
umardani97 <32365705+umardani97@users.noreply.github.com>
2018-04-03 00:31:31 +0800

    membuat atribut dan behavior

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktek02;
 
-/**
- *
- * @author JKendil
- */
+package praktek02;
 public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakInfo(){
+        System.out.println("==================");
+        System.out.println("Panjang :"+panjang);
+        System.out.println("Lebar   :"+lebar);
+        System.out.println("==================");
+    }
 }
